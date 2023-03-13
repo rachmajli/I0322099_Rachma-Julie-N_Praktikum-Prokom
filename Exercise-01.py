b71d455980752fefa254f232221fe284785c7e42
rachmajli <rachmajulienuraini@gmail.com>
2023-03-13 15:09:12 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,14 @@
+nama = 'Rachma Julie Nuraini'
+NIM = 'I0322099'
+asal = 'Magetan'
+visualisasi = 'kabupaten kecil yang berada di kaki Gunung Lawu, berbatasan dengan Jawa Tengah, setiap musim hujan udaranya selalu dingin. Industri yang terkenal dari Kabupaten Magetan adalah industri kerajinan kulit'
+harapan = 'ialah bekerja di bidang yang sesuai dengan mata kuliah yang diajarkan selama di Teknik Industri dan sesuai minat dan cita cita selama ini'
+print()
+
+print('Nama saya', nama)
+print('NIM', NIM)
+print('')
+print('Saya berasal dari', asal)
+print('Magetan merupakan', visualisasi)
+print('')
+print('Harapan saya setelah lulus dari Teknik Industri', harapan)
